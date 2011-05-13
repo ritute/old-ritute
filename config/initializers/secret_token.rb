cde79abd06423e5d6f4155d1b4895b35e22e60a5
rita <rita@rita-think.(none)>
2011-05-13 15:01:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ritute::Application.config.secret_token = 'b97575f6122b9c6f293e86c4d68707605cae6f80bc3791febce9bd256638dd91e8d7b14052c46c352971542047da0af88806862274a2a73b6eb4aa9dc579e0d8'
